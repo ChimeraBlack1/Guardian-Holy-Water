4d8357eaac96f3b4789a6dc8eb396a5660557bc0
ChimeraBlack1 <chimerablaque@hotmail.com>
2017-07-01 08:38:03 -0400

    added holy water descriptions and fixed horizontal scroll

--- src/components/water.js
+++ src/components/water.js
@@ -5,10 +5,15 @@ export default class Water extends Component {
 		
 
 		return(
-			<div className="">
+			<div className="container">
 				<p>Our water is the best water for getting rid of demons and evil spirits.  This water is blessed using our patented “triple holiness” blessing technique.</p>
 				<div className="holyWaterBottle"></div>
 				<h3 className="H3">The "Triple Holiness" technique.</h3>
+				<p className="tripHoly">The water comes from a spring high in thte andes mountains.  It is harvested by a blind priest who brings it down to his village where it sits in a barrel
+Inside a church for 10 years.  From there it is shipped to Israel where it is blessed by a deaf priest.  Once he has blessed the water it is then shipped
+to the Vatican to be blessed by the Pope.  At this stage the tears of a virgin alter boy are added and it is shipped here to Canada for one final blessing.
+Once it is in our Canadian warehouse, our very own mute Priest places the final blessing upon the barrel.  We know it’s ready when it gives off a glow
+that you can only see if you look very closely and believe in the power of almighty god.</p>
 			</div>
 		);
 	}
